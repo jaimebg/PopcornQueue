7e0c1ad4ef694e322daa772c0fad502d7a2e3df4
Jaime Barreto <yaimesil@gmail.com>
2025-07-10 19:34:46 +0100

    fixed ios statusbar color

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar, useColorScheme, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './screens/HomeScreen';
@@ -8,16 +8,32 @@ function App() {
   const Stack = createNativeStackNavigator();
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar translucent={true} backgroundColor={'transparent'} barStyle="dark-content" hidden={false} />
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-          <Stack.Screen name="MovieDetails" component={MovieDetails} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+    <View style={styles.rootContainer}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <SafeAreaView style={styles.safeArea}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+            <Stack.Screen name="MovieDetails" component={MovieDetails} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    backgroundColor: '#1C1C1E',
+  },
+  safeArea: {
+    flex: 1,
+  }
+});
 
 export default App;
